f7ef295e2cba56f107f752173dbdf6a6d62eb017
Bhargav Rao C V <95969426+CeeVeeRao108@users.noreply.github.com>
2022-06-23 10:11:16 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <header>
+            <img
+                src="https://www.pngall.com/wp-content/uploads/7/Smiley-Emoticon-PNG-Image.png"
+                alt="Problem?"
+            />
+            <p>Meme-O-nator</p>
+        </header>
+    )
+}
+
+export default Header
